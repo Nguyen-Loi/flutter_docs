4df83affd39e3c6d9f801931dcc58a9fd02388e6
NguyenLoi <loi.nguyen.lifetechvn@gmail.com>
2024-02-29 15:57:46 +0700

    second commit

---
+++ lib/src/features/onboarding/presentation/onboarding_controller.dart
@@ -0,0 +1,22 @@
+// import 'dart:async';
+
+// import 'package:rivedpod_clone/src/features/onboarding/data/onboarding_repository.dart';
+// import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+
+// part 'onboarding_controller.g.dart';
+
+// @riverpod
+// class OnboardingController extends _$OnboardingController {
+//   @override
+//   FutureOr<void> build() {
+//     // no op
+//   }
+
+//   Future<void> completeOnboarding() async {
+//     final onboardingRepository =
+//         ref.watch(onboardingRepositoryProvider).requireValue;
+//     state = const AsyncLoading();
+//     state = await AsyncValue.guard(onboardingRepository.setOnboardingComplete);
+//   }
+// }
